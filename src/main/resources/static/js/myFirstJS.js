70a6f8914026b141b82760be6b4cd88d4496c67a
alberto soto <alberto.soto@gmail.com>
2019-02-26 15:27:31 +0100

    asd

--- src/main/resources/static/js/myFirstJS.js
+++ src/main/resources/static/js/myFirstJS.js
@@ -1,12 +1,39 @@
+var sillyData = ["hola", "don", "pepito"];
 var commonFunctions = {
+    mydata: ["hola", "don", "pepito"],
     helloWorld: function () {
         alert("VOLAD!");
     },
-    askMeSomething:function () {
-        if (confirm("Estas seguro NEN??")){
+    askMeSomething: function () {
+        if (confirm("Estas seguro NEN??")) {
             alert("Toma truño");
-        }else{
+        } else {
             alert("ME ABURROOOOO");
+        }
+    },
+    askMeBiconditional: function () {
+        // a? b:c;
+        confirm("¿Me bicondicionas??") ? alert("yuhu") : alert("no yuhu");
+    },
+    showMessage: function () {
+        var msg = document.getElementById("messageAlert");
+        console.log(msg);
+        msg.style.display = "inline";
+    },
+    blockMe: function () {
+        for (i = 0; i < 10; i++) {
+            alert("Alert " + i);
+        }
+    },
+    alertInput:function(id){
+      alert(document.getElementById(id).value);
+    },
+    isTrue: function (e) {
+        return e < 10;
+    },
+    seMeVaLaPinza: function () {
+        for (i = 0; this.isTrue(e); i++) {
+            alert("Alert " + i);
         }
     }
 };
