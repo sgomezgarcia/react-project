72d4c078effd12a05f26b502eea468a568617322
Alberto Soto <alberto.soto@gmail.com>
2019-02-27 20:49:38 +0100

    asd

---
+++ js-sandbox/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title= "Hi pepito";
+
+ReactDOM.render(
+  <h1>{title}</h1>,
+  document.getElementById('app')
+);
+
+
+console.log("hi react");
